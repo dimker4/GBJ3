84f6032f630585cfb67a0ddfa2a4b423ec965636
dimker <dimker4@gmail.com>
2020-07-21 17:19:55 +0500

    generics

--- src/main/java/Lesson_1/Main.java
+++ src/main/java/Lesson_1/Main.java
@@ -1,7 +1,27 @@
 package Lesson_1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        String[] arrs = {"aa", "bb", "cc", "dd"};
+        Integer[] arri = {1, 2, 3, 4, 5};
+        ArrayList<String> myList = new ArrayList<String>();
+
+        System.out.println(Arrays.toString(arrs)); // Выведем исходный массив
+        System.out.println(Arrays.toString(arri));
+        UltimateArray<String> ultArrs = new UltimateArray<String>(arrs); // Создадим новый экзмепляр с указанием Типа
+        UltimateArray<Integer> ultArri = new UltimateArray<Integer>(arri); // Создадим новый экзмепляр с указанием Типа
+
+        ultArrs.swap(0, 1); // Меняем местами
+        ultArri.swap(3, 4);
+        System.out.println(Arrays.toString(arrs)); // Выведем измененный массив
+        System.out.println(Arrays.toString(arri));
+
+        myList = ultArrs.convertToList(); // Получаем список из массива
+        System.out.println(myList);
     }
 }
+
+
