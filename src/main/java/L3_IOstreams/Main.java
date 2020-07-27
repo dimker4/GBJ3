abce9c1546663e02da607cf6e58c92e13f486d89
dimker <dimker4@gmail.com>
2020-07-27 12:58:28 +0500

    #0

--- src/main/java/L3_IOstreams/Main.java
+++ src/main/java/L3_IOstreams/Main.java
@@ -1,4 +1,14 @@
 package L3_IOstreams;
 
+import java.util.Arrays;
+import java.util.stream.*;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
+        int count = 0;
+        count = (int) Arrays.stream(numbers).filter(o -> o > 0).count();
+
+        System.out.println(count);
+    }
 }
